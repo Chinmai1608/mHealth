68f0023bdfadc4bb9204408ba73f922c0d2a7fca
Chinmai Nimgade <chinmainimgade1608@gmail.com>
2022-12-31 17:41:07 +0530

    Navbar Edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,30 +4,31 @@ import { ContactPage, Feed, Info, Message, Person } from '@mui/icons-material';
 const Navbar = () => {
   return (
     <div>
-      <navbar class="flex flex-row bg-gradient-to-tr from-white via-white to-rose-100">
-        <div class="flex justify-between  w-full">  
+      <navbar class="flex flex-row bg-dna bg-right-top  ">
+        <div class="flex justify-between  w-full bg-gradient-to-r  from-white via-blue-50 to-transparent">  
         <div class="flex-row flex justify-between w-full pl-8">
             <logo class="text-4xl bg-logobg bg-cover h-24 w-24 px-2">
               <div class="mx-16 my-7 font-bold font-serif">  
-                <div class=" bg-clip-text bg-gradient-to-b from-red-700 to-red-600 "><sup class="text-2xl">M</sup>Health</div>
+                <div class=" "><sup class="text-2xl">M</sup>Health</div>
               </div>
             </logo>
 
             <div class="flex justify-between py-4 px-4 ">
               <item2 class="text-xl px-3 flex flex-col justify-center ">
-                <div class="bg-gradient-to-b from-white via-white to-rose-50 py-2 px-3 rounded-full text-zinc-600">
-                  <Feed/>
+                <div class="backdrop-blur-sm from-white via-white to-rose-50 py-2 px-3 rounded-full text-zinc-600">
+                  <Feed class="w-7 h-7 fill-white"/>
                 </div>
               </item2>
               <item3 class="text-xl px-3 flex flex-col justify-center ">
-              <div class="bg-gradient-to-b from-white via-white to-rose-50 py-2 px-3 rounded-full text-zinc-600">
-                  <ContactPage/>
+              <div class="backdrop-blur-sm from-white via-white to-rose-50 py-2 px-3 rounded-full text-zinc-600">
+                  <ContactPage class="w-7 h-7 fill-white"/>
                 </div>
               </item3>
             </div>
           </div>
         </div>
       </navbar>
+      <hr class="border border-slate-300"/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         logobg : 'url("https://blog.ipleaders.in/wp-content/uploads/2020/01/Health-Insurance.jpg")',
         supplier :'url("https://www.shutterstock.com/image-photo/medicine-pills-capsules-syringe-on-260nw-2169142609.jpg")',
         doctor : 'url("https://media.istockphoto.com/id/1353646476/photo/medical-concept-flat-lay.jpg?b=1&s=170667a&w=0&k=20&c=coIJnw7n7qLqETaN-Vt0HSpmfAIa8KdYZiwnIw3Ccsg=")',
-        dr: 'url("https://i.pinimg.com/736x/20/42/ae/2042aec9a2df42dab4a12d80a884730d.jpg")'
+        home : 'url("https://media.istockphoto.com/id/1156853489/photo/doctor-in-hospital-writing-medical-documentation-and-using-laptop.jpg?s=170667a&w=0&k=20&c=GM_20stSbY7AkiHaHoNGY-coFCKw9DHO5B5XA13SqBw=")',
+        dr: 'url("https://i.pinimg.com/736x/20/42/ae/2042aec9a2df42dab4a12d80a884730d.jpg")',
+        dna : 'url("https://cdn.w600.comps.canstockphoto.com/abstract-dna-spiral-stock-photography_csp48461502.jpg")'
       }
     },
   },
